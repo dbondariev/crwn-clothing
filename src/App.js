995693a60e152fb53a8a4bdccd29ae108a6afd35
Dima Bondariev <dima.bondariev@gmail.com>
2022-12-30 19:49:41 +0200

    simplified html

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 
 
 const App = () => {
+
+  const categoroes = [
+    {
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      id: 1,
+      linkUrl: 'shop/hats'
+    },
+    {
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      id: 2,
+      linkUrl: 'shop/jackets'
+    },
+    {
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      id: 3,
+      linkUrl: 'shop/sneakers'
+    },
+    {
+      title: 'Womans',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womans'
+    },
+    {
+      title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R//Mens.png',
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens'
+    }
+  ]
+
   return (
     <div className="categoroes-containmer">
+      {categoroes.map(({title, imageUrl, id, size}) => (
       <div className="category-container">
         {/*img*/}
         <div classname="category-body-container">
-          <h2>Hats</h2>
+          <h2>{title}</h2>
           <p>Shop Now</p>
         </div>
       </div>
-      <div className="category-container">
-        {/*img*/}
-        <div classname="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>      <div className="category-container">
-        {/*img*/}
-        <div classname="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>      <div className="category-container">
-        {/*img*/}
-        <div classname="category-body-container">
-          <h2>Womans</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>      <div className="category-container">
-        {/*img*/}
-        <div classname="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default App;
